8dfa67bbf84daf44b1394569499cc0cd38113d45
leo <leo@localhost.localdomain>
2019-03-12 03:48:30 -0400

    added cards and dishselection

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {DISHES} from './shared/dishes';
+import {Navbar,NavbarBrand} from 'reactstrap';
 import './App.css';
+import Menu from "./components/MenuComponent";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+  constructor(props) {
+    super(props);
+    this.state={
+      dishes:DISHES
+    }
   }
+
+
+  render() {
+      return(
+          <div>
+              <Navbar dark color="primary">
+                  <div className="container">
+                      <NavbarBrand href="/">Confusion De Ristorante</NavbarBrand>
+                  </div>
+              </Navbar>
+              <div className="container">
+                  <Menu dishes={this.state.dishes}/>
+              </div>
+          </div>
+
+      );
+
+    }
 }
 
 export default App;
